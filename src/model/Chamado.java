06fd4611f95151b7f0c62506bd07205e0641515d
william muller <william-code@hotmail.com>
2017-06-20 00:26:33 -0300

    chamado

--- src/model/Chamado.java
+++ src/model/Chamado.java
@@ -3,15 +3,6 @@
 
 package model;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import javax.swing.JOptionPane;
-
 public class Chamado {
 	
 	private String dataAbertura;
